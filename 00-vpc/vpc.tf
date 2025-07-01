5314844f7fbaa81132145d905ccebd5db0d4aeac
samarareddy <samara.junna@gmail.com>
2025-07-01 14:06:51 +0530

    functions

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -3,8 +3,8 @@ module "vpc" {
     source = "git::https://github.com/SAMARA-REDYY-JUNNA/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
-    public_subnet_cidrs = var.public_subnet_cidrs
-    private_subnet_cidrs = var.private_subnet_cidrs
-    database_subnet_cidrs = var.database_subnet_cidrs
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
     is_peering_required = var.is_peering_required
 }
